ec877cfc36dfd22b0e29380c5edc364ecdbf6318
Sungjun Kim <hzcd108@gmail.com>
2022-06-27 14:39:08 +0900

    2차 세팅

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const port = 6000;
+const app = express();
+const Http = require('http');
+const http = Http.createServer(app);
+
+module.exports = http;
+
+// require('./modules/socket');
+
+// const db = require('./models');
+
+const cors = require('cors');
+const morgan = require('morgan');
+
+// db.sequelize
+//   .sync()
+//   .then(() => console.log('🟢 db 연결 성공'))
+//   .catch(console.error);
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan('dev'));
+
+// app.use('/api', []);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.sendStatus(500);
+});
+
+http.listen(port, () => {
+  console.log('🟢', port, '번 포트');
+});
