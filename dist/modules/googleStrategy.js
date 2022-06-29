fe6c3d5cd3bb9b63c5ce75a27ee01cfdb4cb5911
Sounwoo <mna800@naver.com>
2022-06-29 15:46:24 +0900

    Feat : 소셜 로그인 수정

--- dist/modules/googleStrategy.js
+++ dist/modules/googleStrategy.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const User = require('./user');
+const User = require('../../models/user');
 
 module.exports = () => {
   passport.use(
