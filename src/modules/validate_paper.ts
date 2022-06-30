d597ca54df1cfa8e1789bda37f88755226b7ee1f
Sungjun Kim <hzcd108@gmail.com>
2022-06-30 10:03:13 +0900

    Feat 게시글 유효성 검사 모듈 추가
    Joi로 제목, 내용 유효성 검사

---
+++ src/modules/validate_paper.ts
@@ -0,0 +1,14 @@
+import * as Joi from 'joi';
+
+export function validatePaper() {
+  return Joi.object({
+    title: Joi.string().min(2).max(20).required().messages({
+      'string.min': '제목은 최소 2글자입니다.',
+      'string.max': '제목은 최대 20글자입니다.',
+    }),
+    contents: Joi.string().min(5).max(10000).required().messages({
+      'string.min': '내용은 최소 5글자입니다.',
+      'string.max': '내용은 최대 10000글자입니다.',
+    }),
+  });
+}
