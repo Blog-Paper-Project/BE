b9915789949a9cfe14a164367693e08e63e031b4
MinJi <Niveus128@gmail.com>
2022-07-11 09:11:14 +0900

    예약테이블 코드 수정 완료

--- dist/services/booking.service.js
+++ dist/services/booking.service.js
@@ -1,26 +1,17 @@
 const { idText } = require('typescript');
 const { User, Booking, Leaf } = require('../../models');
+const { findAll } = require('../../models/booking');
 
 //예약 신청
 const createBooking = async (userId, guestId, leaf, hostId, bookingTime, meetingDate) => {
   console.log(14, userId, guestId, leaf, hostId, bookingTime, meetingDate);
-
-  await User.decrement({ point: `${leaf}` }, { where: { userId: guestId } });
-
-  await User.increment({ popularity: `${leaf}` }, { where: { userId: hostId } });
-
-  await Leaf.create({
-    leaf,
-    remarks: '화상채팅 예약',
-    giverId: guestId,
-    recipientId: hostId,
-  });
 
   await Booking.create({
     hostId,
     date: meetingDate,
     time: bookingTime,
     guestId,
+    leaf,
   });
 
   return await Booking.findByPk(guestId);
@@ -45,48 +36,63 @@ const hostInquireBooking = async (userId) => {
 };
 exports.hostInquireBooking = hostInquireBooking;
 
-//나뭇잎 찾기
-const findLeaf = async (giverId) => {
-  return await Leaf.findAll({
-    where: { giverId },
-    order: [['createdAt', 'DESC']],
+const confirmBooking = async (hostId, bookingId, guestId, leaf) => {
+  console.log(hostId, bookingId, guestId, leaf);
+  await Leaf.create({
+    leaf,
+    remarks: '화상채팅 예약',
+    giverId: guestId,
+    recipientId: hostId,
   });
-};
-exports.findLeaf = findLeaf;
 
-const findHost = async (bookingId) => {
+  await User.decrement({ point: leaf }, { where: { userId: guestId } });
+
+  await User.increment({ popularity: leaf }, { where: { userId: hostId } });
+
+  await Booking.update(
+    { accepted: true },
+    {
+      where: { bookingId: bookingId },
+    }
+  );
+
+  return await Booking.findByPk(bookingId);
+};
+exports.confirmBooking = confirmBooking;
+
+//선택 행 정보찾기
+const findOne = async (bookingId) => {
   return await Booking.findAll({
-    where: { bookingId: bookingId },
+    where: { bookingId },
   });
 };
-exports.findHost = findHost;
+exports.findOne = findOne;
 
-//예약취소 : 취소 후 나뭇잎 반환
-const checkBooking = async (guestId, hostId) => {
+const findstaus = async (guestId, hostId) => {
   return await Booking.findAll({
-    where: {
-      guestId: Number(guestId),
-      hostId: Number(hostId),
-    },
-    order: [['createdAt', 'DESC']],
+    where: { guestId },
   });
 };
-exports.checkBooking = checkBooking;
+exports.findstaus = findstaus;
 
-const cancelBooking = async (bookingId, guestId, hostId, giverId, leaf) => {
-  console.log(bookingId, guestId, hostId, giverId, leaf);
-  await Booking.destroy({
-    where: {
-      bookingId: bookingId,
-    },
-  });
-  User.increment({ point: Number(...leaf) }, { where: { userId: Number(...hostId) } });
-  User.decrement({ popularity: Number(...leaf) }, { where: { userId: guestId } });
-  return await Leaf.create({
-    leaf: Number(...leaf),
+// 게스트 예약 취소
+const cancelBooking = async (bookingId, guestId, hostId, leaf) => {
+  console.log(bookingId, guestId, hostId, leaf);
+  await Booking.update(
+    { accepted: false },
+    {
+      where: { bookingId: bookingId },
+    }
+  );
+  User.increment({ point: leaf }, { where: { userId: hostId } });
+  User.decrement({ popularity: leaf }, { where: { userId: guestId } });
+  await Leaf.create({
+    leaf,
     remarks: '화상채팅 취소',
-    giverId: Number(...hostId),
+    giverId: hostId,
     recipientId: guestId,
   });
+
+  return await Booking.findByPk(bookingId);
 };
 exports.cancelBooking = cancelBooking;
