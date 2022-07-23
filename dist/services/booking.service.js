b64074d851573da8d1a7f1995722fadab2b43993
MinJi <Niveus128@gmail.com>
2022-07-23 15:28:26 +0900

    Fix: 예약 신청로직 수정

--- dist/services/booking.service.js
+++ dist/services/booking.service.js
@@ -37,8 +37,8 @@ const findRev = async (hostId, bookingTime, meetingDate) => {
 exports.findRev = findRev;
 
 //예약 신청
-const createBooking = async (blogId, leaf, bookingTime, meetingDate, hostId) => {
-  await User.decrement({ point: leaf }, { where: { blogId: blogId } });
+const createBooking = async (blogId, leaf, bookingTime, meetingDate, hostId, userId) => {
+  await User.decrement({ point: leaf }, { where: { userId: userId } });
   await Leaf.create({
     leaf,
     remarks: '화상채팅 예약',
