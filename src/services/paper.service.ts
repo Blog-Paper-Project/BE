fe0a780d7331b6a446181d79966cafd8bcc5123f
Sungjun Kim <hzcd108@gmail.com>
2022-07-29 17:13:29 +0900

    Fix: 이미지 업로드 사이즈 조정 외 1건
    개인 페이지 조회 API 응답 데이터에 팔로워 블로그 아이디 전송

--- src/services/paper.service.ts
+++ src/services/paper.service.ts
@@ -105,7 +105,7 @@ export const findUserInfo = async (blogId: string) => {
     attributes: ['blogId', 'nickname', 'profileImage', 'introduction', 'popularity'],
     include: [
       { model: Paper, include: { model: Tag, attributes: ['name'] } },
-      { model: User, as: 'Followers', attributes: ['nickname'] },
+      { model: User, as: 'Followers', attributes: ['blogId'] },
     ],
     order: [[Paper, 'createdAt', 'DESC']],
   });
