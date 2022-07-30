4c11bbefded5591747ec8cc414bba7a1de67f994
Sungjun Kim <hzcd108@gmail.com>
2022-07-30 11:33:18 +0900

    Fix: 유효성 검사 제목 최대 30글자로 수정

--- src/modules/paper_validater.ts
+++ src/modules/paper_validater.ts
@@ -2,10 +2,10 @@ import * as Joi from 'joi';
 
 export function validatePaper() {
   return Joi.object({
-    title: Joi.string().min(2).max(20).required().messages({
+    title: Joi.string().min(2).max(30).required().messages({
       'any.required': '제목을 입력해주세요',
       'string.min': '제목은 최소 2글자 이상입니다.',
-      'string.max': '제목은 최대 20글자 이하입니다.',
+      'string.max': '제목은 최대 30글자 이하입니다.',
     }),
     contents: Joi.string().min(5).max(10000).required().messages({
       'any.required': '내용을 입력해주세요',
