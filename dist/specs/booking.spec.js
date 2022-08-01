338e9e842f33ca503abf0a7cd81212ee428a7d0c
mj-song00 <niveus128@gmail.com>
2022-08-01 21:05:23 +0900

    test:testCode 완료

--- dist/specs/booking.spec.js
+++ dist/specs/booking.spec.js
@@ -4,6 +4,7 @@ const app = require('../../app');
 
 //require('dotenv').config();
 
+//날짜를 2주뒤로 바꿔주세요
 beforeAll(async () => {
   await sequelize.sync({ force: true }); // 가짜 ORM 생성
 });
@@ -46,8 +47,8 @@ describe('회원가입 테스트', () => {
       .post('/user/signup')
       .send({
         email: 'c@c.com',
-        nickname: 'mincho2',
-        blogId: 'mincho2',
+        nickname: 'yaya',
+        blogId: 'yaya',
         password: 'asdf1234',
         confirmPassword: 'asdf1234',
       })
@@ -57,12 +58,12 @@ describe('회원가입 테스트', () => {
       });
   });
 
-  it('호스트 로그인', (done) => {
+  it('게스트2 로그인', (done) => {
     request(app)
       .post('/user/login')
-      .send({ email: 'a@a.com', password: 'asdf1234' })
+      .send({ email: 'c@c.com', password: 'asdf1234' })
       .then((res) => {
-        token = 'Bearer ' + res.body.token;
+        btoken = 'Bearer ' + res.body.token;
         userId = res.body.userId;
         blogId = res.body.blogId;
         expect(res.status).toBe(200);
@@ -76,6 +77,19 @@ describe('회원가입 테스트', () => {
       .send({ email: 'b@b.com', password: 'asdf1234' })
       .then((res) => {
         atoken = 'Bearer ' + res.body.token;
+        userId = res.body.userId;
+        blogId = res.body.blogId;
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
+
+  it('호스트 로그인', (done) => {
+    request(app)
+      .post('/user/login')
+      .send({ email: 'a@a.com', password: 'asdf1234' })
+      .then((res) => {
+        token = 'Bearer ' + res.body.token;
         userId = res.body.userId;
         blogId = res.body.blogId;
         expect(res.status).toBe(200);
@@ -105,11 +119,11 @@ describe('예약신청', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 05 2022 13:00:00 GMT+0900',
         end: 'Tue Aug 05 2022 14:00:00 GMT+0900',
       })
-      .set('Authorization', token)
+      .set('Authorization', atoken)
       .then((res) => {
         hostId = res.body.bookingResult.hostId;
         bookingId = res.body.bookingResult.bookingId;
@@ -123,11 +137,11 @@ describe('예약신청', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 01 2022 08:00:00 GMT+0900',
         end: 'Tue Aug 01 2022 09:00:00 GMT+0900',
       })
-      .set('Authorization', token)
+      .set('Authorization', atoken)
       .then((res) => {
         expect(res.text).toContain('이미 지나간 시간대에는 예약할 수 없습니다.');
         expect(res.status).toBe(400);
@@ -151,11 +165,11 @@ describe('예약신청', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 04 2022 08:00:00 GMT+0900',
         end: 'Tue Aug 04 2022 09:00:00 GMT+0900',
       })
-      .set('Authorization', token)
+      .set('Authorization', atoken)
       .then((res) => {
         expect(res.text).toContain('보유한 나뭇잎이 부족합니다.');
         expect(res.status).toBe(400);
@@ -182,11 +196,11 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 03 2022 08:00:00 GMT+0900',
         end: 'Tue Aug 03 2022 09:00:00 GMT+0900',
       })
-      .set('Authorization', token)
+      .set('Authorization', atoken)
       .then((res) => {
         hostId = res.body.bookingResult.hostId;
         bookingId = res.body.bookingResult.bookingId;
@@ -200,11 +214,11 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho2',
+        blogId: 'mincho1',
         start: 'Tue Aug 04 2022 15:00:00 GMT+0900',
         end: 'Tue Aug 04 2022 16:00:00 GMT+0900',
       })
-      .set('Authorization', token)
+      .set('Authorization', atoken)
       .then((res) => {
         hostId = res.body.bookingResult.hostId;
         bookingId = res.body.bookingResult.bookingId;
@@ -218,11 +232,11 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 04 2022 16:00:00 GMT+0900',
         end: 'Tue Aug 04 2022 17:00:00 GMT+0900',
       })
-      .set('Authorization', token)
+      .set('Authorization', atoken)
       .then((res) => {
         hostId = res.body.bookingResult.hostId;
         bookingId = res.body.bookingResult.bookingId;
@@ -233,14 +247,15 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
       });
   });
   it('신청 성공-4', (done) => {
+    request(app);
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
-        start: 'Tue Aug 01 2022 17:00:00 GMT+0900',
-        end: 'Tue Aug 01 2022 18:00:00 GMT+0900',
+        blogId: 'mincho1',
+        start: 'Tue Aug 04 2022 17:00:00 GMT+0900',
+        end: 'Tue Aug 04 2022 18:00:00 GMT+0900',
       })
-      .set('Authorization', token)
+      .set('Authorization', atoken)
       .then((res) => {
         hostId = res.body.bookingResult.hostId;
         bookingId = res.body.bookingResult.bookingId;
@@ -272,7 +287,7 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 04 2022 19:00:00 GMT+0900',
         end: 'Tue Aug 04 2022 20:00:00 GMT+0900',
       })
@@ -290,7 +305,7 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 02 2022 09:00:00 GMT+0900',
         end: 'Tue Aug 02 2022 10:00:00 GMT+0900',
       })
@@ -308,7 +323,7 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 02 2022 10:00:00 GMT+0900',
         end: 'Tue Aug 02 2022 11:00:00 GMT+0900',
       })
@@ -326,7 +341,7 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 02 2022 12:00:00 GMT+0900',
         end: 'Tue Aug 02 2022 13:00:00 GMT+0900',
       })
@@ -344,7 +359,7 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 02 2022 13:00:00 GMT+0900',
         end: 'Tue Aug 02 2022 14:00:00 GMT+0900',
       })
@@ -362,7 +377,7 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 02 2022 14:00:00 GMT+0900',
         end: 'Tue Aug 02 2022 15:00:00 GMT+0900',
       })
@@ -376,94 +391,110 @@ describe('예약신청 실패 - 신청횟수 10개 초과', () => {
         done();
       });
   });
-  it('신청 실패 - 호스트 예약갯수 10개 초과', (done) => {
+
+  it('신청 실패 - 게스트 예약갯수 10개 초과', (done) => {
     request(app)
       .post(`/api/booking/${blogId}`)
       .send({
-        blogId: 'mincho',
+        blogId: 'mincho1',
         start: 'Tue Aug 03 2022 16:00:00 GMT+0900',
         end: 'Tue Aug 03 2022 17:00:00 GMT+0900',
       })
       .set('Authorization', atoken)
       .then((res) => {
-        expect(res.text).toContain('예약 받을 수 있는 횟수를 초과하였습니다');
+        expect(res.text).toContain('예약 가능 횟수를 초과하였습니다');
+        expect(res.status).toBe(400);
+        done();
+      });
+  });
+
+  it('신청 실패 - 호스트 예약신청 10개 초과', (done) => {
+    request(app)
+      .post(`/api/booking/${blogId}`)
+      .send({
+        blogId: 'yaya',
+        start: 'Tue Aug 07 2022 08:00:00 GMT+0900',
+        end: 'Tue Aug 07 2022 09:00:00 GMT+0900',
+      })
+      .set('Authorization', btoken)
+      .then((res) => {
+        expect(res.text).toContain('예약 받을 수 있는 횟수를 초과하였습니다.');
         expect(res.status).toBe(400);
         done();
       });
   });
 });
 
-// describe('예약실패2', () => {});
-// it('신청 실패 - 호스트 예약신청 10개 초과', (done) => {
-//   request(app)
-//     .post(`/api/booking/${blogId}`)
-//     .send({
-//       blogId: 'mincho2',
-//       start: 'Tue Aug 07 2022 08:00:00 GMT+0900',
-//       end: 'Tue Aug 07 2022 09:00:00 GMT+0900',
-//     })
-//     .set('Authorization', atoken)
-//     .then((res) => {
-//       hostId = res.body.bookingResult.hostId;
-//       bookingId = res.body.bookingResult.bookingId;
-//       guestId = res.body.bookingResult.guestId;
-//       expect(res.text).toContain('예약 받을 수 있는 횟수를 초과하였습니다.');
-//       expect(res.status).toBe(400);
-//       done();
-//     });
-// });
+describe('예약조회', () => {
+  it('조회 성공', (done) => {
+    request(app)
+      .get('/api/booking')
+      .set('Authorization', token)
+      .then((res) => {
+        expect(res.body.result).toBe(true);
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
+});
 
-// describe('예약조회', () => {
-//   it('조회 성공', (done) => {
-//     request(app)
-//       .get('/api/booking')
-//       .set('Authorization', token)
-//       .then((res) => {
-//         expect(res.body.result).toBe(true);
-//         expect(res.status).toBe(200);
-//         done();
-//       });
-//   });
-// });
+describe('호스트 예약 수락', () => {
+  it('수락 성공', (done) => {
+    request(app)
+      .patch(`/api/booking/${hostId}/${bookingId}`)
+      .set('Authorization', token)
+      .then((res) => {
+        expect(res.body.result).toBe(true);
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
+});
 
-// describe('호스트 예약 수락', () => {
-//   it('수락 성공', (done) => {
-//     request(app)
-//       .patch(`/api/booking/${hostId}/${bookingId}`)
-//       .set('Authorization', token)
-//       .then((res) => {
-//         expect(res.body.result).toBe(true);
-//         expect(res.status).toBe(200);
-//         done();
-//       });
-//   });
-// });
+describe('호스트 예약 취소', () => {
+  it('호스트 취소 성공', (done) => {
+    request(app)
+      .delete(`/api/booking/host/${hostId}/${bookingId}`)
+      .set('Authorization', token)
+      .then((res) => {
+        expect(res.body.result).toBe(true);
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
+});
 
-// describe('호스트 예약 취소', () => {
-//   it('호스트 취소 성공', (done) => {
-//     request(app)
-//       .delete(`/api/booking/host/${hostId}/${bookingId}`)
-//       .set('Authorization', token)
-//       .then((res) => {
-//         expect(res.body.result).toBe(true);
-//         expect(res.status).toBe(200);
-//         done();
-//       });
-//   });
-// });
+describe('게스트 예약 취소', () => {
+  it('예약 성공', (done) => {
+    request(app)
+      .post(`/api/booking/${blogId}`)
+      .send({
+        blogId: 'mincho1',
+        start: 'Tue Aug 09 2022 13:00:00 GMT+0900',
+        end: 'Tue Aug 09 2022 14:00:00 GMT+0900',
+      })
+      .set('Authorization', atoken)
+      .then((res) => {
+        hostId = res.body.bookingResult.hostId;
+        bookingId = res.body.bookingResult.bookingId;
+        guestId = res.body.bookingResult.guestId;
+        expect(res.body.result).toBe(true);
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
 
-// describe('게스트 예약 취소', () => {
-//   it('게스트 예약 취소 성공', (done) => {
-//     request(app)
-//       .delete(`/api/booking/guest/${guestId}/${bookingId}`)
-//       .set('Authorization', atoken)
-//       .then((res) => {
-//         expect(res.body.result).toBe(true);
-//         expect(res.status).toBe(200);
-//         done();
-//       });
-//   });
-// });
+  it('게스트 예약 취소 성공', (done) => {
+    request(app)
+      .delete(`/api/booking/guest/${guestId}/${bookingId}`)
+      .set('Authorization', atoken)
+      .then((res) => {
+        expect(res.body.result).toBe(true);
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
+});
 
 afterAll(async () => {
   // 테이블을 다시 만듬 -> 기존 유저를 초기화
