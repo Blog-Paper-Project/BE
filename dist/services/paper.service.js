356e9047fb4a652c48ed1beceaf11c353e281d61
Sungjun Kim <hzcd108@gmail.com>
2022-08-02 18:51:24 +0900

    Fix: 전체글 조회 API 응답 데이터로 모든 컬럼 추가

--- dist/services/paper.service.js
+++ dist/services/paper.service.js
@@ -77,7 +77,6 @@ exports.findBestPosts = findBestPosts;
 // 전체 게시글 검색
 const findAllPosts = async () => {
     const papers = (await Paper.findAll({
-        attributes: ['postId', 'title', 'contents', 'thumbnail', 'viewCount', 'createdAt'],
         include: [
             { model: User, as: 'Users', attributes: ['blogId', 'nickname', 'profileImage'] },
             { model: User, as: 'Likes', attributes: ['blogId'] },
