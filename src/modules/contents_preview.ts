e342a11e441c1b1ead2f2a62e96541c2510b3bee
Sungjun Kim <hzcd108@gmail.com>
2022-08-03 17:26:41 +0900

    Fix: 내용 미리보기 정규식 수정

--- src/modules/contents_preview.ts
+++ src/modules/contents_preview.ts
@@ -4,7 +4,10 @@ export default function deleteImgUrl(paper: DTO.PaperByLike | Models.Paper) {
       /!\[(.){0,50}\]\(https:\/\/hanghae-mini-project.s3.ap-northeast-2.amazonaws.com\/[0-9]{13}.[a-z]{3,4}\)/g,
       ''
     )
-    .slice(0, 120);
+    .replace(/[#*`(<br></br>)]/g, '')
+    .replace(/[\s]+/g, ' ')
+    .slice(0, 120)
+    .trim();
 
   return Object.assign(paper, { contents });
 }
