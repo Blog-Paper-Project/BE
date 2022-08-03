beaf8972a5dc95a535dbc6dd4df8de1e81f0ac9b
Sungjun Kim <hzcd108@gmail.com>
2022-08-03 18:57:49 +0900

    Fix: 내용 미리보기 글자수 변경 외 1건
    이미지 가로, 세로 최대 사이즈 설정

--- dist/modules/contents_preview.js
+++ dist/modules/contents_preview.js
@@ -5,7 +5,7 @@ function deleteImgUrl(paper) {
         .replace(/!\[(.){0,50}\]\(https:\/\/hanghae-mini-project.s3.ap-northeast-2.amazonaws.com\/[0-9]{13}.[a-z]{3,4}\)/g, '')
         .replace(/[#*`(<br></br>)]/g, '')
         .replace(/[\s]+/g, ' ')
-        .slice(0, 120)
+        .slice(0, 240)
         .trim();
     return Object.assign(paper, { contents });
 }
